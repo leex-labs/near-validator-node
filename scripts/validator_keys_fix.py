b8633cb3a6ff71cb59f2c55d22da45d4d2dfe5e8
Andrew <51726132+hyperleex@users.noreply.github.com>
2022-07-20 00:50:26 +0300

    Create validator_keys_fix.py

---
+++ scripts/validator_keys_fix.py
@@ -0,0 +1,18 @@
+import json
+import os 
+from pathlib import Path
+
+home = Path.home()
+
+with open(home / ".near/validator_key.json", "r") as f:
+    tokens = json.load(f)
+if "private_key" in tokens:
+    tokens["secret_key"] = tokens["private_key"]
+    del tokens["private_key"]
+if "account_id" in tokens:
+    near_username = os.getenv("NEAR_USERNAME")
+    assert near_username is not None, "can't find NEAR_USERNAME env variable"
+    tokens["account_id"] =  "{}.factory.shardnet.near,".format(near_username)
+
+with open(home / ".near/validator_key.json", "w+") as f:
+    json.dump(tokens, f)
