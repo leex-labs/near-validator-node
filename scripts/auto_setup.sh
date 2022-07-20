5166d22c04226c35370b12cd56755b04492dcf40
Andrew <51726132+hyperleex@users.noreply.github.com>
2022-07-20 23:44:58 +0300

    Update auto_setup.sh

--- scripts/auto_setup.sh
+++ scripts/auto_setup.sh
@@ -78,7 +78,6 @@ rm ~/.near/config.json
 wget -O ~/.near/config.json https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/shardnet/config.json
 
 # Запуск ноды 
-cd nearcore
 ./target/release/neard --home ~/.near run
 
 
