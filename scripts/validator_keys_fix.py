73fcba678a984c04837f1bd8bec1049f332900a4
Andrew <51726132+hyperleex@users.noreply.github.com>
2022-07-20 22:38:15 +0300

    Update validator_keys_fix.py

--- scripts/validator_keys_fix.py
+++ scripts/validator_keys_fix.py
@@ -12,7 +12,7 @@ if "private_key" in tokens:
 if "account_id" in tokens:
     near_username = os.getenv("NEAR_USERNAME")
     assert near_username is not None, "can't find NEAR_USERNAME env variable"
-    tokens["account_id"] =  "{}.factory.shardnet.near,".format(near_username)
+    tokens["account_id"] =  "{}.factory.shardnet.near".format(near_username)
 
 with open(home / ".near/validator_key.json", "w+") as f:
     json.dump(tokens, f)
