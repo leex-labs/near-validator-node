806cb14d46d0982b4a78928379df2302feda2951
Andrew <51726132+hyperleex@users.noreply.github.com>
2022-09-05 13:37:36 +0300

    unpacker

--- scripts/deploy_backup.sh
+++ scripts/deploy_backup.sh
@@ -1,9 +1,11 @@
 ALL_BACKUP_DIR=~/backups/
 DATADIR=~/.near
-
+echo "Unpack last backup to near data directory"
 
 if [ -d "$ALL_BACKUP_DIR" ]; then
-    tar -xvf 
+    UNPACK_FILE=$(find $ALL_BACKUP_DIR -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+    echo "File to unpack: $UNPACK_FILE"
+    tar -xzvf $UNPACK_FILE
 else
     echo $ALL_BACKUP_DIR is not created. Check your permissions.
     exit 0
